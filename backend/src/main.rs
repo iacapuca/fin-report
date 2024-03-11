3024fc8fb8d1b31ee7c0759beb8ba0f6b7f88fd1
Pedro Paes <plpbs@poli.br>
2024-03-11 11:42:47 -0300

    Initial commit structure

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use crate::schemas::schema::{create_schema, Schema};
+use actix_cors::Cors;
+use actix_web::{get, middleware, route, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_lab::respond::Html;
+use dotenvy::dotenv;
+use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
+
+mod schemas;
+
+#[get("/graphiql")]
+async fn graphql_playground() -> impl Responder {
+    Html(graphiql_source("/graphql", None))
+}
+
+#[route("/graphql", method = "GET", method = "POST")]
+async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+    let user = data.execute(&st, &()).await;
+    HttpResponse::Ok().json(user)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let schema = std::sync::Arc::new(create_schema());
+    let port = 8888;
+
+    log::info!("Starting on Port: http://localhost:{}", port);
+    log::info!("Playground running on: http://localhost:{}/graphiql", port);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::from(schema.clone()))
+            .service(graphql)
+            .service(graphql_playground)
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
